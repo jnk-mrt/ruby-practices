fee79a8070561b2302502f42aacae502dca221e3
jnk-mrt <junki500c101@yahoo.co.jp>
2023-09-04 17:35:44 +0900

    add fizzbuzz

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizz_buzz
     (1..20).each do |number|
 
-    if number % 15 == 0
+    if number % 3 == 0 && number % 5 == 0
         puts 'FizzBuzz'
     elsif number % 5 == 0
         puts 'Buzz'
