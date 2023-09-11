ee9b1215336ae4f1aeeebc6ca34d843fe2a8f704
jnk-mrt <junki500c101@yahoo.co.jp>
2023-09-11 16:39:22 +0900

    trim indent

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,15 +1,14 @@
 def fizz_buzz
     (1..20).each do |number|
-
-    if number % 3 == 0 && number % 5 == 0
+      if number % 3 == 0 && number % 5 == 0
         puts 'FizzBuzz'
-    elsif number % 5 == 0
+      elsif number % 5 == 0
         puts 'Buzz'
-    elsif number % 3 == 0
+      elsif number % 3 == 0
         puts 'Fizz'
-    else
+      else
         puts number
-    end
+      end
     end
 end
 fizz_buzz
